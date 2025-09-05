c871b5031a72ae4dc5cee65c2d728758ec971d89
Amin Khattab <aminghost47@gmail.com>
2025-09-05 20:43:56 +0300

    Update CNN.py

--- CNN.py
+++ CNN.py
@@ -4,60 +4,63 @@ import tensorflow as tf
 from keras.preprocessing import image
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(rescale=1. / 255,
-                                   shear_range=0.2,
-                                   zoom_range=0.2,
+train_Datagen = ImageDataGenerator(rescale = 1./255,
+                                   shear_range =0.2,
+                                   zoom_range = 0.2,
                                    horizontal_flip=True)
-
-train_set = train_datagen.flow_from_directory('dataset/training_set',
-                                              target_size=(64, 64),
+train_set = train_Datagen.flow_from_directory('dataset/training_set',
+                                              target_size=(64,64),
                                               batch_size=32,
-                                              class_mode="binary"
-                                              )
+                                              class_mode="binary")
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
-test_set = test_datagen.flow_from_directory("dataset/test_set",
-                                            target_size=(64, 64),
+test_Datagen = ImageDataGenerator(rescale=1./255)
+test_set = test_Datagen.flow_from_directory('dataset/test_set',
+                                            target_size=(64,64),
                                             batch_size=32,
-                                            class_mode='binary'
-                                            )
+                                            class_mode="binary")
 
-cnn = tf.keras.models.Sequential()
+cnn = keras.models.Sequential()
 
-cnn.add(keras.layers.Conv2D(kernel_size=3, filters=32, activation="relu", input_shape=[64, 64, 3]))
-cnn.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
+cnn.add(keras.layers.Conv2D(kernel_size=3 , activation="relu", filters=32, input_shape=[64,64,3]))
+cnn.add(keras.layers.MaxPool2D(pool_size=2,strides=2))
 
-cnn.add(keras.layers.Conv2D(kernel_size=3, filters=32, activation="relu"))
+cnn.add(keras.layers.Conv2D(kernel_size=3,activation="relu",filters=32))
 cnn.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
 
 cnn.add(keras.layers.Flatten())
 
-cnn.add(keras.layers.Dense(units=128, activation="relu"))
-cnn.add(keras.layers.Dense(units=1, activation="sigmoid"))
+cnn.add(keras.layers.Dense(activation="relu",units=128))
+cnn.add(keras.layers.Dense(activation="sigmoid",units=1))
 
-cnn.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-cnn.fit(x=train_set, validation_data=test_set, epochs=25)
+cnn.compile(optimizer="adam",metrics=["accuracy"],loss="binary_crossentropy")
+cnn.fit(x =train_set,validation_data = test_set, epochs = 25 )
 
-test_image_1 = image.load_img("dataset/single_prediction/dog.jpg", target_size=(64, 64))
+test_image_1 = image.load_img("dataset/single_prediction/dog.jpg", target_size=(64,64))
 test_image_1 = image.img_to_array(test_image_1)
-test_image_1 = np.expand_dims(test_image_1, axis=0)
+test_image_1 = np.expand_dims(test_image_1,axis=0)
 result_1 = cnn.predict(test_image_1)
-train_set.class_indices
 
-if result_1[0][0] == 1:
+class_indices = train_set.class_indices
+
+print(train_set.class_indices)
+
+if result_1[0][0] > 0.5:
     prediction_1 = "dog"
 else:
     prediction_1 = "cat"
 
-test_image_2 = image.load_img("dataset/single_prediction/dog.jpg", target_size=(64, 64))
+test_image_2 = image.load_img("dataset/single_prediction/image-79322-800.jpg",target_size=(64,64))
 test_image_2 = image.img_to_array(test_image_2)
 test_image_2 = np.expand_dims(test_image_2, axis=0)
 result_2 = cnn.predict(test_image_2)
-train_set.class_indices
 
-if result_2[0][0] == 1:
+class_indices = train_set.class_indices
+
+if result_2[0][0] > 0.5:
     prediction_2 = "dog"
 else:
     prediction_2 = "cat"
 
-print("so for the first picture i think its", prediction_1, "then for the second image i think its", prediction_2)
+print(train_set.class_indices)
+
+print("okay i think that the first image is ",prediction_1,"and the second image is ",prediction_2)
