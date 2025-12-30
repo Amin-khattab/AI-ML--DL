605db935cc23479618c070be635c2f8161be0791
Amin Khattab <aminghost47@gmail.com>
2025-12-30 21:12:01 +0300

    Update ANN(DL-Classification.py

--- ANN(DL-Classification.py
+++ ANN(DL-Classification.py
@@ -1,40 +1,39 @@
-import numpy as np 
-import matplotlib.pyplot as plt 
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder,OneHotEncoder,StandardScaler
-from sklearn.compose import ColumnTransformer
+import numpy as np
+import keras
 from sklearn.model_selection import train_test_split
-import keras as keras
+from sklearn.preprocessing import OneHotEncoder,LabelEncoder,StandardScaler
+from sklearn.compose import ColumnTransformer
 from sklearn.metrics import confusion_matrix
 
+
 dataset = pd.read_csv("Churn_Modelling.csv")
-x = dataset.iloc[:,3:-1].values
+
+x = dataset.iloc[:, 3:-1].values
 y = dataset.iloc[:,-1].values
 
+lb = LabelEncoder()
+x[:,2] = lb.fit_transform(x[:,2])
 
-encoder = LabelEncoder()
-x[:,2] = encoder.fit_transform(x[:,2])
-
-cl = ColumnTransformer(transformers=[("amin",OneHotEncoder(),[1])],remainder="passthrough")
+cl = ColumnTransformer([("Amin",OneHotEncoder(),[1])],remainder="passthrough")
 x = cl.fit_transform(x)
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.25,random_state=0)
+x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=0,test_size=0.25)
 
 sc = StandardScaler()
 
 x_train_scaled = sc.fit_transform(x_train)
-x_test_scaled = sc.transform(x_test)
+x_test_scaled = sc.fit_transform(x_test)
 
-ann = keras.models.Sequential()
-ann.add(keras.layers.Dense(activation="relu",units=6))
-ann.add(keras.layers.Dense(activation="relu",units=6))
-ann.add(keras.layers.Dense(activation="sigmoid",units=1))
+ann = keras.Sequential()
 
-ann.compile(optimizer='adam',metrics=['accuracy'],loss='binary_crossentropy')
+ann.add(keras.layers.Dense(activation="relu", units=10))
+ann.add(keras.layers.Dense(activation="relu", units=10))
+ann.add(keras.layers.Dense(activation="sigmoid", units=1))
 
-ann.fit(x_train_scaled,y_train,batch_size=1000,epochs=100)
+ann.compile(optimizer="adam",loss="binary_crossentropy", metrics=["accuracy"])
+ann.fit(x_train_scaled,y_train, batch_size=16,epochs=20)
 
 y_pred = ann.predict(x_test_scaled)
-y_pred_labels = (y_pred > 0.5).astype(int)
-print(confusion_matrix(y_test, y_pred_labels))
-
+y_pred_labels = (y_pred>0.5).astype("int")
+print(confusion_matrix(y_test,y_pred_labels))
