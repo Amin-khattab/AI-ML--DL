9fa2513b7573ff57aa992b63682a51b6a9bacc80
Amin Khattab <aminghost47@gmail.com>
2026-04-25 17:36:02 +0300

    Enhance CNN with early stopping and regularization
    Added early stopping and L2 regularization to CNN layers. Adjusted rotation range and batch shuffling.

--- 06_A-modern-CNN.py
+++ 06_A-modern-CNN.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import keras
-from keras.src.layers import Activation
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.datasets import cifar10
-from keras.callbacks import ReduceLROnPlateau
+from keras.callbacks import ReduceLROnPlateau , EarlyStopping
+from keras.regularizers import l2
+import zipfile
+from google.colab import files
+
 
 (x_train,y_train),(x_test,y_test ) = cifar10.load_data()
 
 train_datagen = ImageDataGenerator(
     rescale= 1/255,
     horizontal_flip= True,
-    rotation_range= 0.1,
+    rotation_range= 10,
     width_shift_range= 0.1,
     height_shift_range= 0.1,
     zoom_range=0.05
@@ -27,37 +30,69 @@ train_set = train_datagen.flow(
 )
 
 test_set = test_datagen.flow(
+
     x_test,
     y_test,
-    batch_size=64
+    batch_size=64,
+    shuffle=False
 )
 
 tracker= ReduceLROnPlateau(
-    monitor="val_accuracy",
+    monitor="val_loss",
     patience=3,
     factor=0.5,
     min_lr=0.0001
 )
 
+early = EarlyStopping(
+    patience=7,
+    monitor="val_loss",
+    restore_best_weights=True
+)
+
 cnn = keras.Sequential()
 
-cnn.add(keras.layers.Conv2D(kernel_size=3,filters=32,padding="same",activation = None, input_shape=(32,32,3)))
+#first layer
+
+cnn.add(keras.layers.Conv2D(kernel_size=3,filters=32,padding="same",activation = None,kernel_regularizer=l2(1e-4),use_bias=False,input_shape=(32,32,3)))
 cnn.add(keras.layers.BatchNormalization())
 cnn.add(keras.layers.Activation("relu"))
-cnn.add(keras.layers.Dropout(0.25))
+
+cnn.add(keras.layers.Conv2D(kernel_size=3,filters=32,padding="same",activation = None,kernel_regularizer=l2(1e-4),use_bias=False))
+cnn.add(keras.layers.BatchNormalization())
+cnn.add(keras.layers.Activation("relu"))
+
 cnn.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
 
-cnn.add(keras.layers.Conv2D(kernel_size=3,filters=64,padding="same",activation = None))
+cnn.add(keras.layers.Dropout(0.1))
+
+#second layer
+
+cnn.add(keras.layers.Conv2D(kernel_size=3,filters=64,padding="same",activation = None,kernel_regularizer=l2(1e-4),use_bias=False))
 cnn.add(keras.layers.BatchNormalization())
 cnn.add(keras.layers.Activation("relu"))
-cnn.add(keras.layers.Dropout(0.25))
+
+cnn.add(keras.layers.Conv2D(kernel_size=3,filters=64,padding="same",activation = None,kernel_regularizer=l2(1e-4),use_bias=False))
+cnn.add(keras.layers.BatchNormalization())
+cnn.add(keras.layers.Activation("relu"))
+
 cnn.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
 
-cnn.add(keras.layers.Conv2D(kernel_size=3,filters=128,padding="same",activation = None))
+cnn.add(keras.layers.Dropout(0.1))
+
+#third layer
+
+cnn.add(keras.layers.Conv2D(kernel_size=3,filters=128,padding="same",activation = None,kernel_regularizer=l2(1e-4),use_bias=False))
 cnn.add(keras.layers.BatchNormalization())
 cnn.add(keras.layers.Activation("relu"))
-cnn.add(keras.layers.Dropout(0.25))
+
+cnn.add(keras.layers.Conv2D(kernel_size=3,filters=128,padding="same",activation = None,kernel_regularizer=l2(1e-4),use_bias=False))
+cnn.add(keras.layers.BatchNormalization())
+cnn.add(keras.layers.Activation("relu"))
+
 cnn.add(keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+cnn.add(keras.layers.Dropout(0.1))
 
 cnn.add(keras.layers.GlobalAveragePooling2D())
 
@@ -66,5 +101,5 @@ cnn.add(keras.layers.Dropout(0.5))
 
 cnn.add(keras.layers.Dense(units=10,activation="softmax"))
 
-cnn.compile(optimizer="Adam", loss="sparse_categorical_crossentropy",metrics=["accuracy"])
-cnn.fit(x = train_set,validation_data=test_set,epochs = 10,callbacks = tracker)
+cnn.compile(optimizer=keras.optimizers.Adam(learning_rate=5e-4),loss="sparse_categorical_crossentropy",metrics=["accuracy"])
+cnn.fit(x = train_set,validation_data=test_set,epochs = 40,callbacks = [early,tracker])
